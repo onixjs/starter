f329a748dc4b5f2540e637bc7144666c808f56a0
Jonathan <jcasarrubias@mean.expert>
2018-05-06 23:12:47 -0500

    Release 1.0.1 :rocket
    - Updated @onixjs/core to alpha 27
    - Implemented websocket adapters

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
 import {HostBoot} from '@onixjs/core';
+import {WSAdapter} from '@onixjs/core/dist/src/adapters/ws.adapter';
 import * as path from 'path';
 const config = require('../onixjs.config.json');
 // Create a new HostBoot instance for our OnixJS Config
 const boot: HostBoot = new HostBoot(config, {
   cwd: path.join(process.cwd(), 'dist'),
   port: 9000,
+  adapters: {
+    websocket: WSAdapter,
+  },
 });
 // Run our OnixJS Context
 boot.run().then(() => console.log('LOADED'), e => console.log(e));
